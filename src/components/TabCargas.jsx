665d66368288875a23ded5950c23bd0085bc5fcb
Leandro_CCB <kofleo@gmail.com>
2026-09-03 16:36:27 -0300

    Update TabCargas.jsx

--- src/components/TabCargas.jsx
+++ src/components/TabCargas.jsx
@@ -29,6 +29,7 @@ const HTML = `<div id="tab-cargas" class="page">
           <option value="ROSA">Rosa</option>
           <option value="PINHEIRO">Pinheiro</option>
           <option value="LIROMILS">Liromils</option>
+          <option value="IMPERIO">Imperio</option>
         </select>
       </div>
       <div class="form-group">
@@ -215,6 +216,7 @@ const HTML = `<div id="tab-cargas" class="page">
             <option value="ROSA">Rosa</option>
             <option value="PINHEIRO">Pinheiro</option>
             <option value="LIROMILS">Liromils</option>
+            <option value="IMPERIO">Imperio</option>
           </select>
         </div>
         <div class="form-group"><label>Transportadora</label>
